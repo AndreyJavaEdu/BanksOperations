fd15aaa0dac578a70f1628d4d2cd5e14b5f89fc5
andrey <scream12346@yandex.ru>
2024-04-19 01:18:24 +0300

    Реализовал метод списания средств с карты - pay().

--- src/main/java/com/kamenskiy/io/DebitCard.java
+++ src/main/java/com/kamenskiy/io/DebitCard.java
@@ -1,15 +1,21 @@
 package com.kamenskiy.io;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.List;
 
-public class DebitCard extends BankCard{
+public class DebitCard extends BankCard {
     public DebitCard(BigDecimal balance) {
         super(balance);
     }
 
     @Override
     public boolean pay(BigDecimal amount) {
+        if (amount.compareTo(balance) <= 0) {
+            balance = balance.subtract(amount, new MathContext(3));
+            return true;
+        }
         return false;
     }
 

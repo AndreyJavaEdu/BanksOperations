74132035e628beb80eeb40b075f2f9a3b92889f8
andrey <scream12346@yandex.ru>
2024-04-21 01:44:01 +0300

    Реализованы методы pay() в классе BonusCreditCard - добавлена реализация добавления бонусных баллов. Добавлена реализация метода getAvailableFundsInfo() класса BonusCreditCard.

--- src/main/java/com/kamenskiy/io/bonusCard/BonusCreditCard.java
+++ src/main/java/com/kamenskiy/io/bonusCard/BonusCreditCard.java
@@ -3,6 +3,9 @@ package com.kamenskiy.io.bonusCard;
 import com.kamenskiy.io.CreditCard;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BonusCreditCard extends CreditCard {
     private double bonusPointsRate; //процентная ставка по которой расчитываются бонусные баллы
@@ -28,6 +31,28 @@ public class BonusCreditCard extends CreditCard {
         this.bonusPoints = bonusPoints;
     }
 
+    @Override
+    public boolean pay(BigDecimal amount) {
+        boolean operationPay = super.pay(amount);
+        if (operationPay) {
+            bonusPoints = bonusPoints.add(getPointsWhenPay(amount));
+        }
+        return operationPay;
 
+    }
 
+    private BigDecimal getPointsWhenPay(BigDecimal amount) {
+        return amount.multiply(BigDecimal.valueOf(bonusPointsRate)).divide(BigDecimal.valueOf(100), 3, RoundingMode.HALF_DOWN);
+    }
+
+    @Override
+    public Map<String, BigDecimal> getAvailableFundsInfo() {
+        Map<String, BigDecimal> availableFunds = new HashMap<>();
+        availableFunds.put("Баланс, включающий только собственные средства", balance);
+        availableFunds.put("Основные средства, включающие собственные и кредитные средства", getBalanceInfo());
+        availableFunds.put("Кредитный лимит данной кредитной карты", getCREDIT_LIMIT());
+        availableFunds.put("Процентная ставка бонусных баллов:", BigDecimal.valueOf(bonusPointsRate));
+        availableFunds.put("Количество бонусных баллов", bonusPoints);
+        return availableFunds;
+    }
 }

fab9e9956be357f08c7fae8e6aa0aef05b8d677a
andrey <scream12346@yandex.ru>
2024-04-20 17:39:32 +0300

    Тестирование методов класса CashBackCredit. Предварительная реализация юнит тестов.

---
+++ src/test/java/com/kamenskiy/io/bonusCard/CashBackCreditCardTest.java
@@ -0,0 +1,16 @@
+package com.kamenskiy.io.bonusCard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBackCreditCardTest {
+
+    @Test
+    void pay() {
+    }
+
+    @Test
+    void getAvailableFundsInfo() {
+    }
+}

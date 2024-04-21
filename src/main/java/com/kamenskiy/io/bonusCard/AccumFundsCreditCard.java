66068c4a9e0efd9c8a358d753cc2103b7b59f156
andrey <scream12346@yandex.ru>
2024-04-21 14:37:49 +0300

    Реализовал метод putMoney() класса AccumFundsCreditCard.

---
+++ src/main/java/com/kamenskiy/io/bonusCard/AccumFundsCreditCard.java
@@ -0,0 +1,60 @@
+package com.kamenskiy.io.bonusCard;
+
+import com.kamenskiy.io.CreditCard;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+public class AccumFundsCreditCard extends CreditCard {
+    private final double ACCUM_PERCENT = 0.005; //Процент от суммы пополнений
+    private BigDecimal accumFunds; //накопленная сумма
+
+    public AccumFundsCreditCard(BigDecimal balance, BigDecimal CREDIT_LIMIT, BigDecimal accumFunds) {
+        super(balance, CREDIT_LIMIT);
+        this.accumFunds = accumFunds;
+    }
+
+    public double getACCUM_PERCENT() {
+        return ACCUM_PERCENT;
+    }
+
+    public BigDecimal getAccumFunds() {
+        return accumFunds;
+    }
+
+    public void setAccumFunds(BigDecimal accumFunds) {
+        this.accumFunds = accumFunds;
+    }
+
+    @Override
+    public void putMoney(BigDecimal amount) {
+        if (creditPart.compareTo(getCREDIT_LIMIT()) == 0) {
+            balance = balance.add(amount).add(getAccumFundsOfAmount(amount));
+            accumFunds = accumFunds.add(getAccumFundsOfAmount(amount));
+        } else if (creditPart.compareTo(getCREDIT_LIMIT()) < 0 && balance.equals(BigDecimal.ZERO)) {
+            var remainingDebt = getCREDIT_LIMIT().subtract(creditPart);
+            accumFunds = accumFunds.add(getAccumFundsOfAmount(amount));
+            if (amount.compareTo(remainingDebt) < 0) {
+                creditPart = creditPart.add(remainingDebt).add(getAccumFundsOfAmount(amount));
+            } else if (amount.compareTo(remainingDebt) == 0) {
+                creditPart = creditPart.add(remainingDebt);
+                balance = balance.add(getAccumFundsOfAmount(amount));
+            } else {
+                creditPart = creditPart.add(remainingDebt);
+                balance = balance.add(amount.subtract(remainingDebt));
+                accumFunds = accumFunds.add(getAccumFundsOfAmount(amount));
+            }
+        }
+    }
+
+    private BigDecimal getAccumFundsOfAmount(BigDecimal amount) {
+        return amount.multiply(BigDecimal.valueOf(ACCUM_PERCENT).divide(BigDecimal.valueOf(100), 6, RoundingMode.HALF_DOWN));
+    }
+
+
+    @Override
+    public Map<String, BigDecimal> getAvailableFundsInfo() {
+        return super.getAvailableFundsInfo();
+    }
+}
